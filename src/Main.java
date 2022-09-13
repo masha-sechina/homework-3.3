58b0c926e6bcb6794a42429fbd6d2e4cd4985e86
Masha Sechina <mashka_3@mail.ru>
2022-09-13 22:41:58 +0300

    change homework-3.3

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int payment;
-        payment = service.calculate(1_000_000, 9.99 , 12);
+        payment = service.calculate(1_000_000, 9.99, 12);
         System.out.println(payment);
 
-        payment = service.calculate(1_000_000,9.99, 24);
+        payment = service.calculate(1_000_000, 9.99, 24);
         System.out.println(payment);
 
         payment = service.calculate(1_000_000, 9.99, 36);
