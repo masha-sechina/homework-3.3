e89c7fb3ea76ae00b01eeaf0e1e1ac7da5914480
Masha Sechina <mashka_3@mail.ru>
2022-09-20 23:54:29 +0300

    change homework-3.3

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
-    public int calculate (int a , double b,int c){
+    public int calculate (int amount , double interestRate,int creditTerm){
         int y;
-        double percentInMonth = b/12/100 ;
+        double percentInMonth = interestRate/12/100 ;
         double x = 1 + percentInMonth;
-        y = (int) ( a * ((percentInMonth * Math.pow( x , c)) / ( Math.pow( x , c) -1 )) ) ;
+        y = (int) ( amount * ((percentInMonth * Math.pow( x , creditTerm)) / ( Math.pow( x , creditTerm) -1 )) ) ;
 
         return y;
     }
